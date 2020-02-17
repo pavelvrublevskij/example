9c95766a579ef54973ff3632c6e6d2de32d97b2b
pavel.vrublevskij <pavel.vrublevskij@addendum.lt>
2020-02-17 13:34:44 +0200

    generic: compare-map example added

---
+++ java/generic/compare-map/src/lt/asprogramuoju/bean/Person.java
@@ -0,0 +1,29 @@
+package lt.asprogramuoju.bean;
+
+public interface Person extends Comparable<Person> {
+
+    /**
+     * Person identification
+     * @return uniq id
+     */
+    int getId();
+
+    /**
+     * Person first name
+     * @return first name
+     */
+    String getName();
+
+    /**
+     * Person last name
+     * @return last name
+     */
+    String getSurname();
+
+    /**
+     * Person some number. Could be any, i.e. phone number, id card number, queue number and etc
+     * @return number
+     */
+    Long getNumber();
+
+}
---
+++ java/generic/compare-map/src/lt/asprogramuoju/impl/PersonGroup.java
@@ -0,0 +1,68 @@
+package lt.asprogramuoju.impl;
+
+import com.company.bean.*;
+import lt.asprogramuoju.bean.*;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+
+public abstract class PersonGroup<G, E extends List<? extends Person>> extends HashMap<G, E> {
+
+    /**
+     * Set data to group. For doSort {@link #setComparator} should be override
+     * @param group {@link G}
+     * @param type {@link E}
+     * @param doSort when true will call {@link #setComparator}
+     */
+    public void setToGroup(G group, E type, boolean doSort) {
+        if (doSort) {
+            type.sort(setComparator());
+        }
+        this.put(group, type);
+    }
+
+    public abstract Comparator<Person> setComparator();
+
+    /**
+     * Set person to group. This method don't sort students.
+     * For sorting use {@link #fillDataAndSort(G, E)}
+     * @param group group of {@link G}
+     * @param type list of students {@link E}
+     */
+    public void fillData(G group, E type) {
+        this.put(group, type);
+    }
+
+    /**
+     * Set person to group and make sort asc. See interface {@link E}
+     * @param group group of {@link G}
+     * @param type list of students {@link E}
+     */
+    public void fillDataAndSort(G group, E type) {
+        type.sort(setComparator());
+        fillData(group, type);
+    }
+
+    /**
+     * Return list of persons by group name
+     * @param group
+     * @return {@link E}
+     */
+    public E getByGroup(G group) {
+        return this.get(group);
+    }
+
+    /**
+     * Method will return all data from Map without group
+     * @return List of {@link E}
+     */
+    @SuppressWarnings("unchecked")
+    public E getAllData() {
+        List<Person> personList = new ArrayList<>();
+        this.forEach((g, person) -> personList.addAll(person));
+
+        return (E) personList;
+    }
+}
